2220d01d0f32d4988eb9e3b7405e68a86b86d397
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2024-09-13 22:16:22 +0800

    use third party script (Leaflet library) to load and display a map on UI

--- script.js
+++ script.js
@@ -16,6 +16,20 @@ if (navigator.geolocation) {
     function (position) {
       const { latitude, longitude } = position.coords;
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13);
+
+      //   L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png', {
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');
