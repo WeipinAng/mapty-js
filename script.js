95050a500ddf6702a4fc07b0ab2cd99bfc8fd85a
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2024-09-13 22:52:39 +0800

    create and display marker & pop-up whenever map is clicked upon (bind an event handler to access coordinates of the point clicked)

--- script.js
+++ script.js
@@ -26,10 +26,10 @@ if (navigator.geolocation) {
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng]).addTo(map).bindPopup('Workout').openPopup();
+      });
     },
     function () {
       alert('Could not get your position');
